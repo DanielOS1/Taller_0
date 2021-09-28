10c6d187405ac9040db7f833d57512c0e945fcd5
DanielOS1 <89607851+DanielOS1@users.noreply.github.com>
2021-09-28 11:45:01 -0300

    Add files via upload

--- Taller 0/src/taller/Taller0.java
+++ Taller 0/src/taller/Taller0.java
@@ -3,6 +3,7 @@ package taller;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Taller0 {
@@ -13,37 +14,29 @@ public class Taller0 {
 		String contraseña = ""; 
 		int opcion = 0;
 		int posicionCliente = 0;
+		int contadorSalasDeCine = 0;
 		String [] nombres = new String[50];
 		String [] apellidos = new String[50];
 		String [] ruts = new String[50];
 		String [] contraseñas = new String[50];
 		int [] saldos = new int[50];
+		leerArchivoClientes(nombres, apellidos, ruts, contraseñas, saldos, posicionCliente);
 		
-		BufferedReader arch = new BufferedReader(new FileReader("clientes.txt"));
-		String line;
-		while ((line = arch.readLine()) != null) {
-			String [] partes = line.split(",");
-			String nombre = partes[0];
-			String apellido = partes[1];
-			rut = partes[2];
-			contraseña = partes[3];
-			int saldo = Integer.parseInt(partes[4]);
-			
-			nombres[posicionCliente] = nombre;
-			apellidos[posicionCliente] = apellido;
-			ruts[posicionCliente] = rut;
-			contraseñas[posicionCliente] = contraseña;
-			saldos[posicionCliente] = saldo;
-			
-			posicionCliente++;
-			
-		}
+		String [] nombrePeliculas = new String[50];
+		String [] tiposDePelicula = new String[50];
+		int [] recaudacionPeliculas = new int[50];
+		String [] numerosDeSalas = new String[50];
+		String [] horariosDePeliculas = new String[50];
+		int [] cantFunciones = new int[50];
+		
+		leerArchivoPeliculas(contadorSalasDeCine,posicionCliente, nombrePeliculas, tiposDePelicula, recaudacionPeliculas, numerosDeSalas, horariosDePeliculas, cantFunciones);
+		
 		
 		//Iniciar secion -------->
 		System.out.println("ingrese su rut");
 		rut = iniciarSecion(rut,2);
 		
-		while (!leerArchivoClientes(rut, 2)) {
+		while (!archivoClientesIniciarSecion(rut, 2)) {
 			System.out.println("informacion incorrecta, ingrese un rut valido: ");
 			rut = iniciarSecion(rut,2);
 		}
@@ -51,7 +44,7 @@ public class Taller0 {
 		System.out.println("ingrese su contraseña");
 		contraseña = iniciarSecion(contraseña,3);
 		
-		while (!leerArchivoClientes(contraseña, 3)) {
+		while (!archivoClientesIniciarSecion(contraseña, 3)) {
 			System.out.println("informacion incorrecta, ingrese una contraseña valida");
 			contraseña = iniciarSecion(contraseña,3);
 		}
@@ -60,14 +53,13 @@ public class Taller0 {
 		System.out.println("Bienvenido al menu del cliente. \nPorfavor ingrese una opcion \n1) Compra de entrada \n2) informacion del usuario \n3) devolución \n4) cartelera");
 		opcion = scan.nextInt();
 		scan.nextLine();
-		menuCliente(opcion);
-		
+		menuCliente(opcion,nombrePeliculas,numerosDeSalas,horariosDePeliculas,cantFunciones);
 	}
 	
 public static String iniciarSecion(String parametro, int parametro0) throws Exception {
 	Scanner scan = new Scanner(System.in);
 	parametro = scan.nextLine();
-	if (leerArchivoClientes(parametro, parametro0)) {
+	if (archivoClientesIniciarSecion(parametro, parametro0)) {
 		System.out.println("correcto");
 }
 
@@ -76,7 +68,7 @@ return parametro;
 }
 	
 
-public static boolean leerArchivoClientes(String parametro0,int parametro1) throws Exception{
+public static boolean archivoClientesIniciarSecion(String parametro0,int parametro1) throws Exception{
 	BufferedReader arch = new BufferedReader(new FileReader("clientes.txt"));
 	String line;
 	while ((line = arch.readLine()) != null) {
@@ -89,10 +81,81 @@ public static boolean leerArchivoClientes(String parametro0,int parametro1) thro
 		
 	}return false;
 	
+	
 }
-public static void menuCliente(int parametro0) {
+public static void leerArchivoClientes(String [] lista0, String [] lista1, String [] lista2, String [] lista3, int [] lista4,int contador) throws Exception {
+	BufferedReader arch = new BufferedReader(new FileReader("clientes.txt"));
+	String line;
+	while ((line = arch.readLine()) != null) {
+		String [] partes = line.split(",");
+		String nombre = partes[0];
+		String apellido = partes[1];
+		String rut = partes[2];
+		String contraseña = partes[3];
+		int saldo = Integer.parseInt(partes[4]);
+		
+		lista0[contador] = nombre;
+		lista1[contador] = apellido;
+		lista2[contador] = rut;
+		lista3[contador] = contraseña;
+		lista4[contador] = saldo;
+		
+		contador++;
+		
+	}
+	
+}
+public static void leerArchivoPeliculas(int sumador,int contador,String [] lista0,String [] lista1,int [] lista2,String [] lista3,String [] lista4,int [] lista5) throws Exception {
+	BufferedReader arch = new BufferedReader(new FileReader("peliculas.txt"));
+	String line;
+	while ((line = arch.readLine()) != null) {
+		String [] partes = line.split(",");
+		String nombrePelicula = partes[0];
+		String tipoPelicula = partes[1];
+		int recaudacion = Integer.parseInt(partes[2]);
+		for (int i = 3;i<partes.length;i += 2) {
+			String numeroSala = partes[i];
+			String horarioPelicula = partes[i+1];
+			lista3[sumador] = numeroSala;
+			lista4[sumador] = horarioPelicula;
+			
+			sumador++;
+			
+		}
+		lista0[contador] = nombrePelicula;
+		lista1[contador] = tipoPelicula;
+		lista2[contador] = recaudacion;
+		lista5[contador] = partes.length-3;
+		contador++;
+		
+	}
+
+}
+public static void menuCliente(int parametro0,String [] lista0,String [] lista1,String [] lista2, int [] lista3) {
+	Scanner scan = new Scanner(System.in);
 	 if (parametro0 == 1){
 	        System.out.println("Usted eligio la opcion 1 \nCompra de entradas: ");
+	        System.out.println("ingrese el nombre de la pelicula para desplegar los horarios: ");
+	        String nombrePelicula = scan.nextLine();
+	        
+	        for (int i = 0; i < lista0.length; i++){
+				if(nombrePelicula.equals(lista0[i])){
+					System.out.println("Pelicula encontrada!!, usted eligio la pelicula "+lista0[i]);
+					System.out.println("los horarios disponibles son:");
+					if(i==0){
+						for (int j = 0; j < lista3[0]/2; j++) {
+							System.out.println(lista1[j]+ " "+lista2[j]);
+						}
+						
+					} else {
+					for(int j = lista3[i];j < lista3[j] ;j++) {
+					System.out.println(lista1[j]+ " "+lista2[j]);
+					}
+					break;
+					}
+				}
+			}
+	        
 	        
 	    }
 	    if (parametro0 == 2){
